5d4c3c076c9415a83a3306b65a3aac5dc9d78af8
athirah111-gh <gibbsxx17@gmail.com>
2026-03-20 02:10:32 +0800

    Add files via upload
    Created for 100 Days of Code™: The Complete Python Pro Bootcamp (udemy)
    Day 1: 19 Mar 2026

---
+++ 1-Band Name Generator.py
@@ -0,0 +1,12 @@
+#Band Name Generator - 100-days-of-python
+#Date created: 19 Mar 2026
+
+print("Welcome to Band Name Generator. \n")
+
+city = input("What is your dream city to visit?\n")
+pet_name = input("What is your pet's name? \n")
+
+print(city + " " + pet_name + " sounds good for a band...")
+
+
+#suggestion for further learning - add regenerate, maybe can include genre to mix-n-match name generator
